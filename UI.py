d8d71b7eb51d05feb5d74e2470047045600a467a
sharan-s-pai <sharanpai2001@gmail.com>
2022-08-27 22:11:50 +0530

    UI-changes-and-states-integration

--- UI.py
+++ UI.py
@@ -8,28 +8,19 @@ import plotly.express as px
 import pandas as pd 
 import json
 
+def name_list():
+    li=['India']
+    df=pd.read_csv('./crime-data/india.csv')
+    li=li+list(df['STATE/UT'])
+    # print(li)
+    return li
+
 fig=px.choropleth_mapbox
                        # pip install pandas
 city_list={
-    'New York City':[
-        '../nyc-precinct-wise-dataset.csv',
-        '../first-phase/New York/New York Police Precincts.geojson',
-        '../new-york-city.json'
-    ],
-    'Washington DC':[
-        '../wdc-precinct-wise-dataset.csv',
-        '../first-phase/Washington DC/WashingtonDC-precinct.geojson',
-        '../washington-city.json',
-    ],
-    'Seattle': [
-        '../seattle-precinct-wise-dataset.csv',
-        '../first-phase/Seattle/Seattle-prcinct.geojson',
-        '../seattle.json'
-    ],
-    'Boston':[
-        '../boston-precinct-wise-dataset.csv',
-        '../first-phase/Boston/Boston Police Districts.geojson',
-        '../boston.json'
+    'India': [
+        './india-state-1.geojson',
+        './crime-data/india.csv',
     ]
 }
 file=None
@@ -45,8 +36,8 @@ columns2=[]
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 mytitle = dcc.Markdown(children='')
 mygraph = dcc.Graph(figure={})
-dropdown = dcc.Dropdown(options=["New York City", "Washington DC", "Seattle", "Boston"],
-                        value='New York City',  # initial value displayed when page first loads
+dropdown = dcc.Dropdown(options=name_list(),
+                        value='India',  # initial value displayed when page first loads
                         clearable=False)
 age = dcc.Input(placeholder='Enter Age',
                 type='number',value=None)
@@ -87,18 +78,19 @@ def helper1(file):
     if file != None:
         return file
     fileArr=[
-        './crimes/arson.csv', 
-        './crimes/assault.csv',
-        './crimes/fraud.csv',
-        './crimes/gun-violence.csv',
-        './crimes/harrassment.csv',
-        './crimes/motor-vehicle-accidents.csv',
-        './crimes/murder.csv',
-        './crimes/robbery.csv',
-        './crimes/sex-crime.csv',
-        './crimes/drug-overdose-victims.csv',
-        './crimes/Missing-Persons.csv',
-        './crimes/Human-Trafficking.csv'
+        './Crime data-age-related/Accident.csv',
+        './Crime data-age-related/Arson.csv',
+        './Crime data-age-related/Fraud.csv',
+        './Crime data-age-related/Gun Violence.csv',
+        './Crime data-age-related/Harrassment.csv',
+        './Crime data-age-related/Human Trafficking.csv',
+        './Crime data-age-related/Missing.csv',
+        './Crime data-age-related/Murder.csv',
+        './Crime data-age-related/Others.csv',
+        './Crime data-age-related/Public Nuisance.csv',
+        './Crime data-age-related/Robbery.csv',
+        './Crime data-age-related/Sex Crime.csv',
+        './Crime data-age-related/Theft.csv'
     ]
     file=[]
     for path in fileArr:
@@ -109,38 +101,45 @@ def helper1(file):
 def driver(city,age):
     
     global file
-    file=helper1(file)
- 
-    crime_data = pd.read_csv(city_list[city][0])
-    crime_data = crime_data.iloc[:, 1:]
+    if file is None:
+        file=helper1(file)
+    if city=='India':
+        crime_data = pd.read_csv(city_list[city][1])
+    else:
+        crime_data = pd.read_csv('./crime-data/'+city+'.csv')
+    # crime_data = crime_data.iloc[:, 1:]
     crimes = crime_data.columns
     li = []
 
     for df in file:
+        df=pd.DataFrame(df)
         for _, row in df.iterrows():
             key = row.keys()[0]
             if key in crimes:
-                if age >= row['low'] and age <= row['up']:
-                    if key == 'Robbery':
-                        li.append({'Theft': row['Points']})
-                        crime_data['Theft'] = crime_data['Theft']*row['Points']
+                if age >= row['Low'] and age <= row['Up']:
+                    # if key == 'Robbery':
+                    #     li.append({'Theft': row['Points']})
+                    #     crime_data['Theft'] = crime_data['Theft']*row['Points']
                     li.append({key: row['Points']})
                     crime_data[key] = crime_data[key]*row['Points']
                     # print(crime_data.head(1))
                     break
             else:
                 break
-    if 'Others' in crimes:
-        crime_data['Others'] = crime_data['Others']*2.5
-        li.append({'Others': 2.5})
-    if 'Public Nuisance' in crimes:
-        crime_data['Public Nuisance'] = crime_data['Public Nuisance']*2.5
-        li.append({'Public Nuisance': 2.5})
+    # if 'Others' in crimes:
+    #     crime_data['Others'] = crime_data['Others']*2.5
+    #     li.append({'Others': 2.5})
+    # if 'Public Nuisance' in crimes:
+    #     crime_data['Public Nuisance'] = crime_data['Public Nuisance']*2.5
+    #     li.append({'Public Nuisance': 2.5})
     # print(li)
     li1 = []
     crime_data = crime_data.fillna(0)
     for index, row in crime_data.iterrows():
-        li1.append(sum([float(x) for x in row[2:].values]))
+        if row[0]!='TOTAL':
+            li1.append(sum([float(x) for x in row[2:].values]))
+        else:
+            li1.append(0)
     crime_data['total'] = li1
     return crime_data,li
 
@@ -166,13 +165,18 @@ def update_graph(city, age,clickData):
     global allPoints
     global columns2
     if gage==age and gcity==city and clickData!=None:
-        print(clickData['points'][0]['location'])
+        val=None
+        if city=='India':
+            val='STATE/UT'
+        else:
+            val='DISTRICT'
+        # print(clickData['points'][0]['location'])
         for index,row in crime.iterrows():
-            if row['precinct']==clickData['points'][0]['location']:
+            if row[val]==clickData['points'][0]['location']:
                 data=pd.DataFrame(row).transpose()
                 df=pd.DataFrame(row).transpose()
                 break
-        print(pd.DataFrame(allPoints).to_dict('records'))
+        # print(pd.DataFrame(allPoints).to_dict('records'))
         columns2=[]
         res2={}
         for point in allPoints:
@@ -192,19 +196,28 @@ def update_graph(city, age,clickData):
         crime_data,points=driver(city,age)
         print(city)
         print(type(city))
-        data=json.load(open(city_list[city][2]))
+        # data=json.load(open(city_list[city][2]))
         crime=crime_data
         allPoints=points
-        lat=data['latitude']
-        long=data['longitude']
-        precincts=json.load(open(city_list[city][1],'r'))
+        # lat=data['latitude']
+        # long=data['longitude']
+        precincts=None
         # https://plotly.com/python/choropleth-maps/
-
-        fig=px.choropleth_mapbox(crime_data,locations='precinct',featureidkey="properties.precinct",geojson=precincts,color='total',color_continuous_scale='ylorrd')
+        val1=None
+        val2=None
+        if city=='India':
+            precincts=json.load(open(city_list[city][0],'r'))
+            val1='STATE/UT'
+            val2='properties.state_name'
+        else:
+            precincts=json.load(open('./india-state-district.geojson','r'))[city]
+            val1='DISTRICT'
+            val2='properties.district'
+        fig=px.choropleth_mapbox(crime_data,locations=val1,featureidkey=val2,geojson=precincts,color='total',color_continuous_scale='ylorrd',zoom=4)
         fig.update_geos(fitbounds="locations", visible=False)
         fig.update_layout(mapbox_style="carto-positron", 
                         mapbox_zoom=9.5,
-                        mapbox_center={"lat": lat, "lon": long},
+                        mapbox_center={"lat": 20.5937, "lon": 78.9629},
                         margin={"r":0,"t":0,"l":0,"b":0},
                         uirevision='constant')
         figure=fig
